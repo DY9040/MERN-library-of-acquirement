05036b5fa926a73c67470f4a56ae57ce494115a6
Forever Young <dy9040@gmail.com>
2022-06-11 02:46:05 -0500

    updated connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/mern-library-of-acquirement', 
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/MERN-library-of-acquirement', 
   {
   useNewUrlParser: true,
   useUnifiedTopology: true,
