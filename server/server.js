23f1ad67ef31fa74bd5ca11dc5e11fca0c932f7a
Forever Young <dy9040@gmail.com>
2022-06-10 22:44:08 -0500

    edited server file and added apollo and graphQL

--- server/server.js
+++ server/server.js
@@ -3,8 +3,17 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const { ApolloSever } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -19,3 +28,19 @@ app.use(routes);
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+const startApolloServer = async ( typeDefs, resolvers ) => {
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  dbonce('open', () => {
+    app.listen(PORT, () =>{
+      console.log(`API server running on port ${PORT}!`);
+
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+};
+
+startApolloServer( typeDefs, resolvers);
